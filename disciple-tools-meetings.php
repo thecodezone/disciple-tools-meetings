5982cff5ea4b49da717faba4726b3c3755a1e946
corsacca <corsacca@gmail.com>
2021-03-24 18:56:33 +0100

    Set up meeting fields and remove un-needed code.

--- disciple-tools-meetings.php
+++ disciple-tools-meetings.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Plugin Name: Disciple Tools - Meetings
+ * Plugin Name: Disciple Tools - Meetings Proof of Concept
  * Plugin URI: https://github.com/DiscipleTools/disciple-tools-meetings
- * Description: Disciple Tools - Meetings is intended to help developers and integrator jumpstart their extension of the Disciple Tools system.
+ * Description: Track your group meetings.
  * Text Domain: disciple-tools-meetings
  * Domain Path: /languages
  * Version:  0.1
@@ -16,12 +16,6 @@
  * @link    https://github.com/DiscipleTools
  * @license GPL-2.0 or later
  *          https://www.gnu.org/licenses/gpl-2.0.html
- */
-
-/**
- * Refactoring (renaming) this plugin as your own:
- * 3. @todo Update the README.md and LICENSE
- * 4. @todo Update the default.pot file if you intend to make your plugin multilingual. Use a tool like POEdit
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -81,65 +75,14 @@ class Disciple_Tools_Meetings {
     }
 
     private function __construct() {
-        $is_rest = dt_is_rest();
-        /**
-         * @todo Decide if you want to use the REST API example
-         * To remove: delete this following line and remove the folder named /rest-api
-         */
-        if ( strpos( dt_get_url_path(), 'disciple_tools_meetings_template' ) !== false ) {
-            require_once( 'rest-api/rest-api.php' ); // adds starter rest api class
-        }
 
-        /**
-         * @todo Decide if you want to create a new post type
-         * To remove: delete the line below and remove the folder named /post-type
-         */
         require_once( 'post-type/loader.php' ); // add starter post type extension to Disciple Tools system
 
-        /**
-         * @todo Decide if you want to add a custom tile
-         * To remove: delete the line below and remove the folder named /tile
-         */
-        require_once( 'tile/custom-tile.php' ); // add custom tile
-
-        /**
-         * @todo Decide if you want to add a custom admin page in the admin area
-         * To remove: delete the 3 lines below and remove the folder named /admin
-         */
-        if ( is_admin() ) {
-            require_once( 'admin/admin-menu-and-tabs.php' ); // adds starter admin page and section for plugin
-        }
-
-        /**
-         * @todo Decide if you want to support localization of your plugin
-         * To remove: delete the line below and remove the folder named /languages
-         */
         $this->i18n();
 
-        /**
-         * @todo Decide if you want to customize links for your plugin in the plugin admin area
-         * To remove: delete the lines below and remove the function named
-         */
-        if ( is_admin() ) { // adds links to the plugin description area in the plugin admin list.
-            add_filter( 'plugin_row_meta', [ $this, 'plugin_description_links' ], 10, 4 );
-        }
 
     }
 
-    /**
-     * Filters the array of row meta for each/specific plugin in the Plugins list table.
-     * Appends additional links below each/specific plugin on the plugins page.
-     */
-    public function plugin_description_links( $links_array, $plugin_file_name, $plugin_data, $status ) {
-        if ( strpos( $plugin_file_name, basename( __FILE__ ) ) ) {
-            // You can still use `array_unshift()` to add links at the beginning.
-
-            $links_array[] = '<a href="https://disciple.tools">Disciple.Tools Community</a>'; // @todo replace with your links.
-            // @todo add other links here
-        }
-
-        return $links_array;
-    }
 
     /**
      * Method that runs only when the plugin is activated.
@@ -277,20 +220,6 @@ if ( ! function_exists( "dt_hook_ajax_notice_handler" )){
 }
 
 /**
- * Plugin Releases and updates
- * @todo Uncomment and change the url if you want to support remote plugin updating with new versions of your plugin
- * To remove: delete the section of code below and delete the file called version-control.json in the plugin root
- *
- * This section runs the remote plugin updating service, so you can issue distributed updates to your plugin
- *
- * @note See the instructions for version updating to understand the steps involved.
- * @link https://github.com/DiscipleTools/disciple-tools-meetings/wiki/Configuring-Remote-Updating-System
- *
- * @todo Enable this section with your own hosted file
- * @todo An example of this file can be found in (version-control.json)
- * @todo Github is a good option for delivering static json.
- */
-/**
  * Check for plugin updates even when the active theme is not Disciple.Tools
  *
  * Below is the publicly hosted .json file that carries the version information. This file can be hosted
@@ -299,21 +228,21 @@ if ( ! function_exists( "dt_hook_ajax_notice_handler" )){
  * Also, see the instructions for version updating to understand the steps involved.
  * @see https://github.com/DiscipleTools/disciple-tools-version-control/wiki/How-to-Update-the-Starter-Plugin
  */
-//add_action( 'plugins_loaded', function (){
-//    if ( is_admin() ){
-//        // Check for plugin updates
-//        if ( ! class_exists( 'Puc_v4_Factory' ) ) {
-//            if ( file_exists( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' )){
-//                require( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' );
-//            }
-//        }
-//        if ( class_exists( 'Puc_v4_Factory' ) ){
-//            Puc_v4_Factory::buildUpdateChecker(
-//                'https://raw.githubusercontent.com/DiscipleTools/disciple-tools-meetings/master/version-control.json',
-//                __FILE__,
-//                'disciple-tools-meetings'
-//            );
-//
-//        }
-//    }
-//} );
+add_action( 'plugins_loaded', function (){
+    if ( is_admin() ){
+        // Check for plugin updates
+        if ( ! class_exists( 'Puc_v4_Factory' ) ) {
+            if ( file_exists( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' )){
+                require( get_template_directory() . '/dt-core/libraries/plugin-update-checker/plugin-update-checker.php' );
+            }
+        }
+        if ( class_exists( 'Puc_v4_Factory' ) ){
+            Puc_v4_Factory::buildUpdateChecker(
+                'https://raw.githubusercontent.com/DiscipleTools/disciple-tools-meetings/master/version-control.json',
+                __FILE__,
+                'disciple-tools-meetings'
+            );
+
+        }
+    }
+} );
--- post-type/loader.php
+++ post-type/loader.php
@@ -9,30 +9,27 @@ if ( ! class_exists( 'DT_Module_Base' ) ) {
     return;
 }
 
-/**
- * Add any modules required or added for the post type
- */
-add_filter( 'dt_post_type_modules', function( $modules ){
-
-    /**
-     * @todo Update the starter in the array below 'starter_base'. Follow the pattern.
-     * @todo Add more modules by adding a new array element. i.e. 'starter_base_two'.
-     */
-    $modules["starter_base"] = [
-        "name" => "Starter",
-        "enabled" => true,
-        "locked" => true,
-        "prerequisites" => [ "contacts_base" ],
-        "post_type" => "meetings",
-        "description" => "Default starter functionality"
-    ];
-
-    return $modules;
-}, 20, 1 );
+///**
+// * Add any modules required or added for the post type
+// */
+//add_filter( 'dt_post_type_modules', function( $modules ){
+//
+//    /**
+//     * @todo Update the starter in the array below 'starter_base'. Follow the pattern.
+//     * @todo Add more modules by adding a new array element. i.e. 'starter_base_two'.
+//     */
+//    $modules["starter_base"] = [
+//        "name" => "Starter",
+//        "enabled" => true,
+//        "locked" => true,
+//        "prerequisites" => [ "contacts_base" ],
+//        "post_type" => "meetings",
+//        "description" => "Default starter functionality"
+//    ];
+//
+//    return $modules;
+//}, 20, 1 );
 
 require_once 'module-base.php';
 Disciple_Tools_Meetings_Base::instance();
 
-/**
- * @todo require_once and load additional modules
- */
